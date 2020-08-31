2d5d7def723228695fa2feab1fcf9f67fb94deb9
Roman Shemyakin <ramon93i7@gmail.com>
2020-08-31 18:41:14 +0300

    v1.0.0 :)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="module_exports",
-    version="1.0",
+    version="1.0.0",
     author="Roman Shemyakin",
     author_email="ramon93i7@gmail.com",
     description="One decrator to export them all :)",
